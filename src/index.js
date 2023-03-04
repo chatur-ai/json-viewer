731d8fce4b8705b31a21d7942346771730b31a1a
Ajay Viswanathan <ajay.viswanathan@gmail.com>
2023-03-04 22:41:59 +0700

    Adds google analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ReactGA from 'react-ga'
+import App from './App'
+
+const TRACKING_ID = "G-ZHEQ9YL428"
+ReactGA.initialize(TRACKING_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
