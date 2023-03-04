1407252bec0602ebe9e5379c8e0bb3718860d11f
Ajay Viswanathan <ajatix@users.noreply.github.com>
2023-03-04 18:48:14 +0700

    Bootstrap (#1)
    * Get rid of unwanted stuff from create-react-app
    
    * Adds grommet
    
    * Adds a simple json viewer
    
    * Smarter inference of max key usage
    
    * Implements nested card viewer
    
    * Clean up imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Grid, grommet, Grommet, Header, Heading, Text, TextArea } from "grommet"
+import { React, useReducer } from "react"
+import { renderJsonCards } from "./utils"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'input_text': {
+      try {
+        const parsedJson = JSON.parse(action.value)
+        return {
+          textInput: action.value,
+          jsonInput: parsedJson,
+          errors: []
+        }
+      } catch (e) {
+        return {
+          textInput: action.value,
+          jsonInput: undefined,
+          errors: ["Please input a valid JSON string"]
+
+        }
+      }
+    }
+    default: return state
+  }
 }
 
-export default App;
+const initialState = {
+  textInput: undefined,
+  jsonInput: undefined,
+  viewCards: true,
+  viewTable: false,
+  errors: []
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <Grommet full theme={grommet} background="light-2">
+      <Grid
+        rows={['xxsmall', 'auto']}
+        columns={['1/2', '1/2']}
+        areas={[
+          { name: 'header', start: [0, 0], end: [1, 0] },
+          { name: 'textView', start: [0, 1], end: [0, 1] },
+          { name: 'jsonView', start: [1, 1], end: [1, 1] },
+        ]}
+        fill
+      >
+        <Header gridArea="header" background="brand" pad="small">
+          <Heading size="small">JSON Viewer</Heading>
+          <Text size="xsmall">Made with ❤️ by chatur.ai</Text>
+        </Header>
+        <Box gridArea="textView" background="neutral-2" margin="small" round="xsmall">
+          <TextArea placeholder="Enter JSON" value={state.textInput} onChange={event => dispatch({ type: "input_text", value: event.target.value })} fill resize={false} size="small" style={{ fontFamily: 'monospace' }} plain />
+        </Box>
+        <Box gridArea="jsonView" margin="small">
+          {state.jsonInput && renderJsonCards(state.jsonInput)}
+        </Box>
+      </Grid>
+    </Grommet >
+  )
+}
+
+export default App
---
+++ src/utils.js
@@ -0,0 +1,69 @@
+import { Card, CheckBox, DataTable, Grid, NameValueList, NameValuePair, Text } from "grommet"
+import { countBy, extend, flatten, forEach, isArray, isBoolean, isNumber, isString, keys, map, mapObject, max, object, sample, uniq } from "underscore"
+
+const renderString = (datum, key) => <Text>{datum[key]}</Text>
+const renderNumber = (datum, key) => <Text>{datum[key]}</Text>
+const renderBoolean = (datum, key) => <CheckBox checked={datum[key]} />
+const renderArray = (datum, key) => renderJsonTable(datum[key])
+const renderObject = (datum, key) => renderJsonCards(datum[key], true)
+
+export const renderJsonCards = (json, inner) => {
+    const data = isArray(json) ? json : [json] // Object >> Array
+    const extracted = extractKeyMetadata(data)
+    return (
+        <Grid gap="small" columns="fit">
+            {map(data, (datum, index) => {
+                return (
+                    <Card background={inner ? "light-1" : "white"} pad="medium" key={index} >
+                        <NameValueList key={index} layout="grid">
+                            {map(keys(datum), key => {
+                                return (
+                                    <NameValuePair name={key} key={key}>
+                                        {extracted.metadata[key].render(datum, key)}
+                                    </NameValuePair>
+                                )
+                            })}
+                        </NameValueList>
+                    </Card>
+                )
+            })}
+        </Grid>
+    )
+}
+
+export const renderJsonTable = (json) => {
+    const data = isArray(json) ? json : [json] // Object >> Array
+    const extracted = extractKeyMetadata(data)
+    const columns = extractColumns(extracted)
+    return <DataTable columns={columns} data={data} sortable verticalAlign="top" />
+}
+
+const inferRender = (data, key) => {
+    const sampleDatum = sample(data)[key]
+    return sampleDatum ? (isString(sampleDatum) ? renderString : isNumber(sampleDatum) ? renderNumber : isBoolean(sampleDatum) ? renderBoolean : isArray(sampleDatum) ? renderArray : renderObject) : inferRender(data, key)
+}
+
+const extractKeyMetadata = (data) => {
+    const allKeys = uniq(flatten(map(data, datum => keys(datum))))
+    const keyUsage = map(allKeys, key => countBy(data, datum => datum[key] ? 'exists' : 'missing'))
+    const maxKeyUsage = max(keyUsage, usage => usage['exists'] || 0)["exists"] || 0
+    forEach(keyUsage, usage => usage['primary'] = (usage['exists'] || 0) === maxKeyUsage)
+    const keyMetadata = object(allKeys, keyUsage)
+    mapObject(keyMetadata, (metadata, key) => extend(metadata, { render: inferRender(data, key) }))
+    return {
+        keys: allKeys,
+        metadata: keyMetadata
+    }
+}
+const extractColumns = (extracted) => {
+    return map(extracted.keys, function (key) {
+        return {
+            property: key,
+            header: <Text>{key}</Text>,
+            search: false,
+            sortable: true,
+            primary: extracted.metadata[key].primary,
+            render: datum => extracted.metadata[key].render(datum, key)
+        }
+    })
+}
